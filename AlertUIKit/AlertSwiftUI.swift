4f5cecd0b47114ed700508ff8f6fe0ceb7ef135a
Acasiax <117105267+Acasiax@users.noreply.github.com>
2023-12-14 23:51:31 +0900

    Alert창 형식을 UIKit 버전과 SwiftUI 버전을 구현하여 공부했습니다

---
+++ AlertUIKit/AlertSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  AlertSwiftUI.swift
+//  AlertUIKit
+//
+//  Created by 이윤지 on 12/14/23.
+//
+
+import SwiftUI
+
+struct AlertSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertSwiftUI()
+}
